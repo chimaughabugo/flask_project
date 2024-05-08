2a14c81eb60306f34c465645d52389b17ad1a7c0
Chima Ughabugo <chimaughabugo4@gmail.com>
2024-05-08 19:02:14 +0100

    initial commit

---
+++ hello.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+
+#create a flask instance
+app = Flask(__name__)
+
+#Create a Route decorator
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html", user_name=name)
+
+@app.route('/sample')
+def sample():
+    return render_template("example.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
+
+@app.route('/user2/<name>')
+def user2(name):
+    return render_template("user2.html", name=name)
